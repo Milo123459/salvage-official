d900301fd0d86e4900ef16df14b5ba0c6aaf7cd5
Milo123459 <milo5huntington@gmail.com>
2020-06-18 12:44:03 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
     <Router>
     <div className="App">
       <h1>Hey there! Welcome to my site.</h1>
+      Click me to go to the <a href = "/todo">todo</a> page
       <Link to = {`/todo`}/>
     </div>
     <Route path = "/todo" component = {Todo}/>
