da55d141742d411892f83c30b3d7964278e8238b
Milo123459 <milo5huntington@gmail.com>
2020-06-18 11:33:47 +0100

    New update

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,14 @@
 import React from "react";
 export default class User extends React.Component {
     constructor(props){
-        console.log(props)
+        super(props)
     }
     render(){
         return (
             <div>
                 <h1>Name: {this.props.name}</h1>
-                <h2>Language: JavaScript</h2>
-                <p>Job Title: ReactJS Dev</p>
+                <h2>Language: {this.props.name}</h2>
+                <p>Job Title: {this.props.jobTitle}</p>
             </div>
         )
     }
