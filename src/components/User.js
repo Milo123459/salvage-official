26a3aec05d0991f37c776b6236c0de1468a88765
Milo123459 <milo5huntington@gmail.com>
2020-06-18 11:34:49 +0100

    New update

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,7 @@ export default class User extends React.Component {
         return (
             <div>
                 <h1>Name: {this.props.name}</h1>
-                <h2>Language: {this.props.name}</h2>
+                <h2>Language: {this.props.language}</h2>
                 <p>Job Title: {this.props.jobTitle}</p>
             </div>
         )
