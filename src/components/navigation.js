fc3c8ee6aa424b8c5f60f341170a6b9022c38bc9
Milo123459 <milo5huntington@gmail.com>
2020-06-18 10:33:48 +0100

    Initial commit

---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
+  );
+}
+export default Navigation;
---
+++ src/components/register-form.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function RegisterForm(){
+    return (
+        <form>
+            <input name = "username" type = "text" placeholder = "Username"></input>
+        </form>
+    )
+}
