89879a5c7e2eeb47c788678f678cceadfe98742d
Milo123459 <milo5huntington@gmail.com>
2020-06-18 12:39:44 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import User from "./components/User";
 import Post from "./components/Post";
 import BrowseTime from "./components/BrowseTime";
-import { BrowserRouter as Router,  } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import Todo from "./components/Todo";
 const users = [
   {
