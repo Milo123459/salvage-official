4c8fa328ac46009813e45f7a6479f913969b0c0f
Milo123459 <milo5huntington@gmail.com>
2020-06-18 11:39:14 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <User name = "Salvage_Dev" language = "JavaScript" jobTitle = "React.JS"/>
-      <Post title = "Awesome update" content = "I love NodeJS!"></Post>
+      <Post title = "Awesome update" content = "I love NodeJS!"/>
     </div>
   );
 }
