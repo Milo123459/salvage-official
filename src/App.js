9b35789d8425a003de4c5ee182d279a1b802a313
Milo123459 <milo5huntington@gmail.com>
2020-06-18 17:21:21 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
     <div className="App">
-      <h1>Hey there! Welcome to my site.</h1>
-      Click me to go to the <a href = "/todo">todo</a> page
-      <footer>
-        <BrowseTime />
-      </footer>
+      <h1> Welcome to my site! </h1>
     </div>
-    <Route path = "/todo" component = {Todo}/>
     </Router>
   );
 }
