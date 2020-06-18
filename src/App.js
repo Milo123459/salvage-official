b4232b644e26944dde698d2a023127a9b45ff5bb
Milo123459 <milo5huntington@gmail.com>
2020-06-18 17:17:23 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import User from "./components/User";
-import Post from "./components/Post";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Todo from "./components/Todo";
-const users = [
-  {
-    name: "Salvage_Dev",
-    language: "JavaScript",
-    jobTitle: "React.JS dev"
-  }
-]
-const posts = [
-  {
-    title: "My first programming language",
-    content: "My favourite web framework is React.JS"
-  }
-]
-//Components
-const postComponents = posts.map((post, key) => <Post key = {key} title = {post.title} content = {post.content}/>)
-const userComponents = users.map((user, key) => <User key = {key} name = {user.name} language = {user.language} jobTitle = {user.jobTitle}/>)
 function App() {
   return (
     <Router>
