495b47737ad3412fb3b707a943fc7f184ca9c01f
Milo123459 <milo5huntington@gmail.com>
2020-06-18 11:38:55 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import User from "./components/User";
+import Post from "./components/Post";
 function App() {
   return (
     <div className="App">
       <User name = "Salvage_Dev" language = "JavaScript" jobTitle = "React.JS"/>
-      
+      <Post title = "Awesome update" content = "I love NodeJS!"></Post>
     </div>
   );
 }
